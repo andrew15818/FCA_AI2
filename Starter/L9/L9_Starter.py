5480ed26a406774c54b18c191be58d7945755cd0
Andres Ponce <andreseeponce@gmail.com>
2021-02-03 01:17:54 +0800

    added correct L9 Starter file

--- Starter/L9/L9_Starter.py
+++ Starter/L9/L9_Starter.py
@@ -8,15 +8,15 @@ from sklearn.naive_bayes import MultinomialNB
 
 
 def load_data(filename='mail.txt'):
- # A DataFrame is like a giant array, with class and message categories
-    data = DataFrame(columns=['class', 'message'])
-    lines = []
+    # A DataFrame is like a giant array, with class and message categories
+
+
     with open(filename, 'r') as f:
         # Split every line into the message and it's class
-        lines = f.readlines()
-        lines = [line.split('\t') for line in lines]
+        
+       
     # Make an array for easier processing later
-    data  = DataFrame(lines, columns=['class', 'message'])
+    
 
     return data
 
@@ -26,25 +26,26 @@ def main():
     # A vectorizer will remove punctuation from our strings,
     # and count how many times a word appears
     # This makes it harder to fool our system by adding symbols
-    vectorizer = CountVectorizer()
-    counts = vectorizer.fit_transform(data['message'].values)
+
+
+
 
     # Get the answers to use for training
-    targets = data['class'].values
+
     
     # Our classifier now knows which words appear in spam and ham messages
-    classifier = MultinomialNB()
-    classifier.fit(counts, targets)
+    
+
 
     # Time to try it on some new messages!
-    test_data = load_data(filename='test_messages.txt')
+
 
     # make the format similar to our training_data
-    test_labels = test_data['class'].values
-    test_data_vec   = vectorizer.transform(test_data['message'].values)
+
+
 
     # See what our model thinks the test messages are
-    preds = classifier.predict(test_data_vec)
+
     correct = 0
 
     # Print the correct answer and our guess 
