7235972be7a6804054e236a2c1918fe5ca56b80d
Andres Ponce <andreseeponce@gmail.com>
2021-02-18 18:22:00 +0800

    trying new recursive method

--- Starter/L10/L10_Starter.py
+++ Starter/L10/L10_Starter.py
@@ -4,8 +4,7 @@ import numpy as np
 base = 3                    # Rows and columns of board
 elems = base * base         # Total number of elements 
 squares = elems * elems
-#empties = squares * 1 // 2
-empties = 15  # Number of empty boxes
+empties = 10  # Number of empty boxes
 
 def pattern(r, c):
     return (base*(r%base)+r//base+c)%elems
@@ -31,8 +30,8 @@ def create_board():
 # Check if all numbers appear only once
 # type refers to whether we check row/col or block
 def check_all_numbers(arr, type='1d'):
-    # TODO: Check if the 3x3 block doesn't repeat 
-    count = [0] * (elems + 1) # Keep track of the numbers we've seen
+    # Check a 3x3 block
+    count = [0] * (elems + 1)
     if type == 'block':
         for row in arr:
             for col in row:
@@ -42,8 +41,7 @@ def check_all_numbers(arr, type='1d'):
                 else:
                     count[col] += 1
         return True
-
-    # TODO: check if row/col is valid 
+   
     for i in arr:
         # If a number is repeated
         if (count[i] != 0 
@@ -54,17 +52,10 @@ def check_all_numbers(arr, type='1d'):
             count[i] += 1
     return True
 
-def find_next_point(board):
-    rows, cols = board.shape
-    for i in range(rows):
-        for j in range(cols):
-            if board[i][j] == 0:
-                return (i, j)
 
-    return (-1, -1)
 def is_solved(board):
     
-    # TODO: Check every row and column
+    # Check every row and column
     for i, row in enumerate(board):
         if not check_all_numbers(row):
             return False
@@ -74,11 +65,12 @@ def is_solved(board):
             return False
     
  
-    # TODO: Check every subsquare
+    # Check every subsquare
     for i in range(0, elems, base):
         for j in range(0, elems, base): 
             block = board[i:i+(base),j:j+(base)]
-            if not check_all_numbers(block, type='block'):    
+            if not check_all_numbers(block, type='block'):
+    
                 return False
     # If we make it this far we know it's valid board
     return True
@@ -95,35 +87,17 @@ def print_board(board):
         print('\n|----|----|----|----|----|----|----|----|----|')
 
     pass
-def is_valid(board, i, j, x):
-    pass
-def solve_new(board):
-    if is_solved(board):
-        print("Reached end")
-        return True
-    i, j = find_next_point(board)
-    if (i, j) == (-1, -1):
-        return True
 
-    for tmp in range(1, elems+1):
-        board[i][j] = tmp
-        if solve_new(board):
-            return True
-        board[i][j] = 0
 
-    return False
-# Backtracking search    
 def solve(board):
     if is_solved(board):
-        print("Reached end")
         return 
+
     (rows, cols) = board.shape
-    # TODO: Find the first unsolved location
     for row in range(rows):
         for col in range(cols):
             if board[row][col] == 0: 
                 for i in range(1, elems+1):
-                    # TODO: Skip if num already in same row or col
                     if (i not in board[row] 
                             and i not in board[:,col]):
                         board[row][col] = i
@@ -131,13 +105,6 @@ def solve(board):
     return 
                     
 def main():
-    # Debug code for new method
-    board = create_board()
-    delete_random(board)
-    print_board(board)
-    print(solve_new(board))
-    print_board(board)
-    '''
     board = create_board()
     delete_random(board)
     print('====Initial Board====')
@@ -145,6 +112,5 @@ def main():
     print('====Solved Board====')
     solve(board)
     print_board(board) 
-    '''
 if __name__=='__main__':
     main()
